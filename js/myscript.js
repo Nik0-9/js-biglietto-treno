168824e25f25afeb751fd6f9e1a2506975e2722d
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-06 15:55:26 +0100

    ticket discount done

---
+++ js/myscript.js
@@ -0,0 +1,28 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+let km = (prompt('Inserire il numero di chilometri da percorrere:'));
+let age = parseInt(prompt("Inserire l'età del passeggero:"));
+let price = km * 0.21;
+
+console.log(price);
+console.log(isNaN(km));
+
+if(!isNaN(km)){
+    if(age < 18){
+        price = price - (price * 20 / 100);
+        document.getElementById('ticket').innerHTML = `il prezzo del biglietto è di: ${price} euro`;
+    } else if(age >= 65){
+        price = price - (price * 40 / 100);
+        document.getElementById('ticket').innerHTML = `il prezzo del biglietto è di: ${price} euro`;
+    } else{
+        document.getElementById('ticket').innerHTML = `il prezzo del biglietto è di: ${price} euro`;
+    }
+} else {
+    document.getElementById('ticket').innerHTML = 'non hai inserito un numero nel campo km';
+    
+}
